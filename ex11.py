e4d6f9fe890156929bf55600f2b11b622e8517e9
musabbelahouel <p15182028@alumni365.dmu.ac.uk>
2020-01-18 02:58:33 +0000

    input

---
+++ ex11.py
@@ -0,0 +1,9 @@
+print("How old are you?")
+age = input('Enter Age ')
+print("How tall are you?")
+height = input()
+print("how much do you weigh?")
+weight = input()
+
+print("So, you're %r years old, %r tall and %r heavy. "%
+      (age, height, weight))
